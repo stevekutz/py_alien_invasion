52a77783be9e43e0bec50c6c5d9e6c9ee05e77b0
stevekutz <stkutz@gmail.com>
2020-05-30 20:22:42 -0500

    added ability to move ship left & right

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,6 +17,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height)) 
         pygame.display.set_caption("Alien_Invasion")
 
+        # the self.ship instance is assigned to give Ship access to all game resourses via self parameter
         self.ship = Ship(self)
 
         # set background color
@@ -25,20 +26,46 @@ class AlienInvasion:
     def run_game(self):
         """Start main loop in game"""
         while True:
-            # create event listener to watch keyboard or mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()  # check event listener
+            self.ship.update()  # update position
+            self._update_screen() # refresh screen
 
-            # Redraw screen during each pass through the loop
-            # a surface obj is created via self.screen
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        # create event listener to watch keyboard or mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True  # move ship to the right
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True   # move ship to the left
 
-            # make most recent screen drawn visible
-            pygame.display.flip()
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False   # moving right key released, stop moving
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False   # moving left key released, stop moving        
+
+    def _update_screen(self):
+        """ Update images """
+        # Redraw screen during each pass through the loop
+        # a surface obj is created via self.screen
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Redraw screen during each pass through the loop
+        # a surface obj is created via self.screen
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # make most recent screen drawn visible
+        # updates entire display
+        pygame.display.flip()
 
 if __name__ == '__main__':    
     # make a game instance & run game
+    print("__name__ is", __name__)
+    print("pygame.__name__ is", pygame.__name__)
     ai = AlienInvasion()
     ai.run_game()                
--- ship.py
+++ ship.py
@@ -8,6 +8,10 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
         # Load ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -15,6 +19,14 @@ class Ship:
         # start each new ship at bottom center of surface (e.g. display screen)
         self.rect.midbottom = self.screen_rect.midbottom
 
+    def update(self):
+        """ Update the ship position based on the movement flag  """    
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1    
+
     def blitme(self):
         """ Draw the ship at its current location """
+        # blit() draws one image onto another
         self.screen.blit(self.image, self.rect)
